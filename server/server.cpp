ba23f545c2eda67df447a5d545fb37d9a750c08e
Eduard Vlasenko <ehduardvlasenko+git@yandex.ru>
2016-03-13 17:15:16 +0300

    Class BasicCell

--- server/server.cpp
+++ server/server.cpp
@@ -26,8 +26,34 @@ public:
 
 };
 
+class BasicCell {
+public:
+  Cell() {}
+  virtual void effect(Player &player) {  }
+  virtual void move_effect(int& x, int& y) {  };
+};
+
+
+class ExampleCell: public Cell {
+public:
+  ExampleCell() {}
+  virtual void effect(Player &player) {
+    //
+    move_effect(player.x, player.y); // if not empty
+    std::cout << player.nickname << " moved to (57, 57)" << std::endl;
+  }
+  virtual void move_effect(int& x, int& y) {
+    x = 57;
+    y = 57;
+  };
+};
+
 
 
 int main() {
+  Player player1("player1", 1, 1);
+  ExampleCell cell1;
+  cell1.effect(player1);
+  std::cout << player1.x << player1.y;
   return 0;
 }
