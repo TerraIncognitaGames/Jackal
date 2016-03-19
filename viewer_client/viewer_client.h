cff192f3fcdadce11c713d7c059566fa111ff07f
Rmdr <dirsbox@gmail.com>
2016-03-19 20:21:52 +0300

    change classes

--- viewer_client/viewer_client.h
+++ viewer_client/viewer_client.h
@@ -37,10 +37,12 @@ public:
     StringParser(std::string string) : string_(string) {}
 
     VisualizerInitializingCommand ParseIntoVisualizerInitializingCommand() {
+        VisualizerInitializingCommand command;
         // future realization depends on the form of command
     }
 
     VisualizerUpdatingCommand ParseIntoVisualizerUpdatingCommand() {
+        VisualizerUpdatingCommand command;
         // future realization depends on the form of command
     }
 
@@ -55,53 +57,68 @@ private:
     std::string string_;
 };
 
-// Realizes the drawer.
+// Realizes the drawer using singleton.
 class Visualizer {
 public:
-    Visualizer() {}
-
-    void Initialize(std::string string_command) {
+    static Visualizer & Instance() {
+        static Visualizer visualizer;
+        return visualizer;
+    }
+    static void Initialize(std::string string_command) {
         StringParser parser(string_command);
-        auto command = parser.ParseIntoVisualizerInitializingCommand();
+        //auto command = parser.ParseIntoVisualizerInitializingCommand();
         
     }
-
-    void Update(std::string string_command) {
-        StringParser parser(string_command);
-        auto command = parser.ParseIntoVisualizerUpdatingCommand();
-        
-    }
-
-    void Reset() {}
-    
-    void Visualize() {
+    static void Visualize() {
         myGlutInit();
         glutDisplayFunc(DisplayFunc);
         glutMainLoop();
     }
 private:
-    /* There is some shit below */
+    // Realizes one game field drawn on the computer screen.
+    class GameField {
+
+    };
+
+    static std::vector<GameField> game_fields_;
+
+    Visualizer() {}
+    ~Visualizer() {}
+    Visualizer(const Visualizer & other) {}
+    Visualizer & operator=(const Visualizer & other) {}
+
+    static void Update(std::string string_command) {
+        StringParser parser(string_command);
+        auto command = parser.ParseIntoVisualizerUpdatingCommand();
+
+    }
+
     static void DisplayFunc() {
         glClear(GL_COLOR_BUFFER_BIT);
-        glColor3f(1.0, 0.0, 1.0); //fiol
+        // glColor3f(1.0, 1.0, 1.0);
+
         glFlush();
     }
-    void myGlutInit() {
+    static void myGlutInit() {
         int WinWid = 400;
         int WinHei = 400;
         glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
         glutInitWindowSize(WinWid, WinHei);
-        glutInitWindowPosition(900, 0);
+        
+        //void glutReshapeWindow(int width, int height)// оконный режим
+        glutInitWindowPosition(0, 0);
         glutCreateWindow("Window");
-        glClearColor(1.0, 0.0, 1.0, 0.0);
+        glClearColor(1.0, 1.0, 1.0, 0.0);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);/*выбор режима прорисовки
                                                   полигонов. выбран дефолтный.*/
         glMatrixMode(GL_PROJECTION); //Установить проекцию
         glLoadIdentity();
-        int GameFieldWid = 200;
-        int GameFieldHei = 300;
-        glOrtho(0.0, GameFieldWid, 0.0, GameFieldHei, -1.0, 1.0);
-        //glutKeyboardFunc(myGlutKeyboardFunc);
+        // glutFullScreen(); // полноэкранный режим
+        glutReshapeWindow(1300, 800);
+        // int GameFieldWid = 200;
+        // int GameFieldHei = 300;
+        // glOrtho(0.0, GameFieldWid, 0.0, GameFieldHei, -1.0, 1.0);
+        // glutKeyboardFunc(myGlutKeyboardFunc);
         //glutSpecialFunc(myGlutSpecialFunc);
     }
 };
