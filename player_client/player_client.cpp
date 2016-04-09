66a4a21e68747b2129b2ca998087bdf7346ead95
Strausyatina <mendelevich@179.ru>
2016-04-09 15:39:27 +0300

    client

--- player_client/player_client.cpp
+++ player_client/player_client.cpp
@@ -1,1 +1,60 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <random>
+#include <functional>
+#include <time.h>
+
+#include "socket.h"
+
+
+using std::map;
+using std::string;
+using std::vector;
+
+enum Direction { TOP, BOTTOM, RIGHT, LEFT };
+
+/// классы :
+
+/// pirate
+class Pirate {
+private :
+  bool gold_;
+
+public :
+  bool gold() {
+    return gold;
+  }
+
+}
+/// ship
+
+/// water
+/// arrow
+/// hoarse
+/// ice
+/// crocodile
+
+/// baloon
+/// gun
+
+/// field
+/// jungle
+/// desert
+/// bog
+/// mountain
+
+/// hole
+/// rum
+/// cannibal
+
+/// fortress
+/// aborigine
+
+/// gold
+
+
 
